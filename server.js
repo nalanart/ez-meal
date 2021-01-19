452a0b0708d0bc7be9a392b08d36d7a300b29b2b
Alan Tran <alantran96@gmail.com>
2021-01-19 16:47:57 -0500

    Change path to client/build instead of frontend/build.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if(process.env.NODE_ENV === 'production') {
    app.use(express.static(path.join(__dirname, 'frontend/build')))
    //Handle React routing, return all requests to React app
    app.get('*', function(req, res) {
-     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
+     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
    })
  }
 
