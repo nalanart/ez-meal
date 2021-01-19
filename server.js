0694a038afe9d9ddd000c9df3cc83a817d4ae1b4
Alan Tran <alantran96@gmail.com>
2021-01-19 16:57:46 -0500

    frontend/build to client/build

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('/users', usersRouter)
 
 if(process.env.NODE_ENV === 'production') {
    // Serve any static files
-   app.use(express.static(path.join(__dirname, 'frontend/build')))
+   app.use(express.static(path.join(__dirname, 'client/build')))
    //Handle React routing, return all requests to React app
    app.get('*', function(req, res) {
      res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
